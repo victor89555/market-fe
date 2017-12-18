6c0e289c3f9753d5800380832fbed875d01a149b
chenshixiong <1067816765@qq.com>
2017-12-18 18:31:39 +0800

    统计图

--- src/app/manage/order/order-statistics/order-statistics.component.ts
+++ src/app/manage/order/order-statistics/order-statistics.component.ts
@@ -8,26 +8,195 @@ import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 })
 export class OrderStatisticsComponent implements OnInit {
 
-  chartOption: any
-
+  businessOption: any
+  greensOption:any
+  shopOption:any
   constructor() {
   }
 
   ngOnInit() {
-    this.chartOption = {
-      title: {
-        text: '堆叠区域图'
-      },
+    this.getBusinessStatistics()
+    this.getGreensSellStatistic()
+    this.getShopSellStatistic()
+  }
+
+// 获取营业额统计
+  getBusinessStatistics(){
+    let data = [["2000-06-05",116],["2000-06-06",129],["2000-06-07",135],["2000-06-08",86],["2000-06-09",73],["2000-06-10",85],
+      ["2000-06-11",73],["2000-06-12",68],["2000-06-13",92],["2000-06-14",130],["2000-06-15",245],["2000-06-16",139],
+      ["2000-06-17",115],["2000-06-18",111],["2000-06-19",309],["2000-06-20",206],["2000-06-21",137],["2000-06-22",128],
+      ["2000-06-23",85],["2000-06-24",94],["2000-06-25",71],["2000-06-26",106],["2000-06-27",84],["2000-06-28",93],["2000-06-29",85],
+      ["2000-06-30",73],["2000-07-01",83],["2000-07-02",125],["2000-07-03",107],["2000-07-04",82],["2000-07-05",44],["2000-07-06",72],
+      ["2000-07-07",106],["2000-07-08",107],["2000-07-09",66],["2000-07-10",91],["2000-07-11",92],["2000-07-12",113],["2000-07-13",107],
+      ["2000-07-14",131],["2000-07-15",111],["2000-07-16",64],["2000-07-17",69],["2000-07-18",88],["2000-07-19",77],["2000-07-20",83],
+      ["2000-07-21",111],["2000-07-22",57],["2000-07-23",55],["2000-07-24",60]];
+
+    let dateList = data.map(function (item) {
+      return item[0];
+    });
+    let valueList = data.map(function (item) {
+      return item[1];
+    });
+
+    this.businessOption = {
+
+      // Make gradient line here
+      visualMap: [{
+        show: false,
+        type: 'continuous',
+        seriesIndex: 0,
+        min: 0,
+        max: 400
+      }, {
+        show: false,
+        type: 'continuous',
+        seriesIndex: 1,
+        dimension: 0,
+        min: 0,
+        max: dateList.length - 1
+      }],
+
+
+      title: [{
+        left: 'center',
+        text: '营业额走势图'
+      }, {
+        top: '55%',
+        left: 'center',
+        text: 'Gradient along the x axis'
+      }],
       tooltip: {
         trigger: 'axis'
       },
+      xAxis: [{
+        data: dateList
+      }, {
+        data: dateList,
+        gridIndex: 1
+      }],
+      yAxis: [{
+        splitLine: {show: false}
+      }, {
+        splitLine: {show: false},
+        gridIndex: 1
+      }],
+      grid: [{
+        bottom: '60%'
+      }, {
+        top: '60%'
+      }],
+      series: [{
+        type: 'line',
+        showSymbol: false,
+        data: valueList
+      }, {
+        type: 'line',
+        showSymbol: false,
+        data: valueList,
+        xAxisIndex: 1,
+        yAxisIndex: 1
+      }]
+    };
+  }
+
+//获取菜品销量统计
+  getGreensSellStatistic(){
+    this.greensOption = {
+      title : {
+        text: '菜品销量分布图',
+        subtext: '',
+        x:'center'
+      },
+      tooltip : {
+        trigger: 'item',
+        formatter: "{a} <br/>{b} : {c} ({d}%)"
+      },
       legend: {
-        data: ['邮件营销', '联盟广告', '视频广告', '直接访问', '搜索引擎']
+        x : 'center',
+        y : 'bottom',
+        data:['rose1','rose2','rose3','rose4','rose5','rose6','rose7','rose8']
       },
       toolbox: {
-        feature: {
-          saveAsImage: {}
+        show : true,
+        feature : {
+          mark : {show: true},
+          dataView : {show: true, readOnly: false},
+          magicType : {
+            show: true,
+            type: ['pie', 'funnel']
+          },
+          restore : {show: true},
+          saveAsImage : {show: true}
         }
+      },
+      calculable : true,
+      series : [
+        {
+          name:'半径模式',
+          type:'pie',
+          radius : [20, 110],
+          center : ['25%', '50%'],
+          roseType : 'radius',
+          label: {
+            normal: {
+              show: false
+            },
+            emphasis: {
+              show: true
+            }
+          },
+          lableLine: {
+            normal: {
+              show: false
+            },
+            emphasis: {
+              show: true
+            }
+          },
+          data:[
+            {value:10, name:'rose1'},
+            {value:5, name:'rose2'},
+            {value:15, name:'rose3'},
+            {value:25, name:'rose4'},
+            {value:20, name:'rose5'},
+            {value:35, name:'rose6'},
+            {value:30, name:'rose7'},
+            {value:40, name:'rose8'}
+          ]
+        },
+        { name:'面积模式',
+          type:'pie',
+          radius : [30, 110],
+          center : ['75%', '50%'],
+          roseType : 'area',
+          data:[
+            {value:10, name:'rose1'},
+            {value:5, name:'rose2'},
+            {value:15, name:'rose3'},
+            {value:25, name:'rose4'},
+            {value:20, name:'rose5'},
+            {value:35, name:'rose6'},
+            {value:30, name:'rose7'},
+            {value:40, name:'rose8'}
+          ]
+        }
+      ]
+    };
+  }
+
+//获取商户销量排名
+  getShopSellStatistic(){
+
+    // app.title = '堆叠柱状图';
+    this.shopOption = {
+      tooltip : {
+        trigger: 'axis',
+        axisPointer : {            // 坐标轴指示器，坐标轴触发有效
+          type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+        }
+      },
+      legend: {
+        data:['直接访问','邮件营销','联盟广告','视频广告','搜索引擎','百度','谷歌','必应','其他']
       },
       grid: {
         left: '3%',
@@ -35,62 +204,84 @@ export class OrderStatisticsComponent implements OnInit {
         bottom: '3%',
         containLabel: true
       },
-      xAxis: [
+      xAxis : [
         {
-          type: 'category',
-          boundaryGap: false,
-          data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+          type : 'category',
+          data : ['周一','周二','周三','周四','周五','周六','周日']
         }
       ],
-      yAxis: [
+      yAxis : [
         {
-          type: 'value'
+          type : 'value'
         }
       ],
-      series: [
+      series : [
         {
-          name: '邮件营销',
-          type: 'line',
-          stack: '总量',
-          areaStyle: {normal: {}},
-          data: [120, 132, 101, 134, 90, 230, 210]
+          name:'直接访问',
+          type:'bar',
+          data:[320, 332, 301, 334, 390, 330, 320]
         },
         {
-          name: '联盟广告',
-          type: 'line',
-          stack: '总量',
-          areaStyle: {normal: {}},
-          data: [220, 182, 191, 234, 290, 330, 310]
+          name:'邮件营销',
+          type:'bar',
+          stack: '广告',
+          data:[120, 132, 101, 134, 90, 230, 210]
         },
         {
-          name: '视频广告',
-          type: 'line',
-          stack: '总量',
-          areaStyle: {normal: {}},
-          data: [150, 232, 201, 154, 190, 330, 410]
+          name:'联盟广告',
+          type:'bar',
+          stack: '广告',
+          data:[220, 182, 191, 234, 290, 330, 310]
         },
         {
-          name: '直接访问',
-          type: 'line',
-          stack: '总量',
-          areaStyle: {normal: {}},
-          data: [320, 332, 301, 334, 390, 330, 320]
+          name:'视频广告',
+          type:'bar',
+          stack: '广告',
+          data:[150, 232, 201, 154, 190, 330, 410]
         },
         {
-          name: '搜索引擎',
-          type: 'line',
-          stack: '总量',
-          label: {
-            normal: {
-              show: true,
-              position: 'top'
-            }
-          },
-          areaStyle: {normal: {}},
-          data: [820, 932, 901, 934, 1290, 1330, 1320]
+          name:'搜索引擎',
+          type:'bar',
+          data:[862, 1018, 964, 1026, 1679, 1600, 1570],
+          markLine : {
+            lineStyle: {
+              normal: {
+                type: 'dashed'
+              }
+            },
+            data : [
+              [{type : 'min'}, {type : 'max'}]
+            ]
+          }
+        },
+        {
+          name:'百度',
+          type:'bar',
+          barWidth : 5,
+          stack: '搜索引擎',
+          data:[620, 732, 701, 734, 1090, 1130, 1120]
+        },
+        {
+          name:'谷歌',
+          type:'bar',
+          stack: '搜索引擎',
+          data:[120, 132, 101, 134, 290, 230, 220]
+        },
+        {
+          name:'必应',
+          type:'bar',
+          stack: '搜索引擎',
+          data:[60, 72, 71, 74, 190, 130, 110]
+        },
+        {
+          name:'其他',
+          type:'bar',
+          stack: '搜索引擎',
+          data:[62, 82, 91, 84, 109, 110, 120]
         }
       ]
-    }
+    };
+
   }
 
 }
