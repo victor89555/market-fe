b29b95295d16dd72e2a7a3225b4b60b20623c94d
Victor <573864600@qq.com>
2017-12-23 16:44:27 +0800

    20171223new 提交 By Victor

--- src/app/manage/report/mounth-report/mounth-report.component.ts
+++ src/app/manage/report/mounth-report/mounth-report.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {MounthReport,Shop} from "../shared/report.model";
+import {Market} from "../../market/shared/market.model";
+import {MarketService} from "../../market/shared/market.service";
+import {ReportService} from "../shared/report.service"
 
 @Component({
   selector: 'app-mounth-report',
@@ -7,9 +11,292 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MounthReportComponent implements OnInit {
 
-  constructor() { }
+
+  constructor(private marketService:MarketService,
+              private reportService: ReportService) { }
+
+  markets: Market[] = []
+  marketId: number = null
+  mounth: string = ''
+  report: MounthReport = {
+    days: ['01/01','01/02','01/03','01/04','01/05','01/06','01/07','01/08','01/09','01/10','01/11','01/12','01/13'],
+    shops: [
+      {
+        shopId: 1,
+        shopName: "王二牛肉店",
+        amounts:[
+          1000,
+          2000,
+          1000,
+          2000,
+          1000,
+          2000,
+          2000,
+          1000,
+          2000,
+          1000,
+          2000,
+          2000,
+          1000
+        ]
+      },
+      {
+        shopId: 2,
+        shopName: "李四牛肉店",
+        amounts:[
+          1000,
+          2000,
+          1000,
+          2000,
+          1000,
+          2000,
+          2000,
+          1000,
+          2000,
+          1000,
+          2000,
+          2000,
+          1000
+        ]
+      }
+      ,
+      {
+        shopId: 3,
+        shopName: "李四牛肉店",
+        amounts:[
+          1000,
+          2000,
+          1000,
+          2000,
+          1000,
+          2000,
+          2000,
+          1000,
+          2000,
+          1000,
+          2000,
+          2000,
+          1000
+        ]
+      },
+      {
+        shopId: 2,
+        shopName: "李四牛肉店",
+        amounts:[
+          1000,
+          2000,
+          1000,
+          2000,
+          1000,
+          2000,
+          2000,
+          1000,
+          2000,
+          1000,
+          2000,
+          2000,
+          1000
+        ]
+      }
+      ,
+      {
+        shopId: 3,
+        shopName: "李四牛肉店",
+        amounts:[
+          1000,
+          2000,
+          1000,
+          2000,
+          1000,
+          2000,
+          2000,
+          1000,
+          2000,
+          1000,
+          2000,
+          2000,
+          1000
+        ]
+      },
+      {
+        shopId: 2,
+        shopName: "李四牛肉店",
+        amounts:[
+          1000,
+          2000,
+          1000,
+          2000,
+          1000,
+          2000,
+          2000,
+          1000,
+          2000,
+          1000,
+          2000,
+          2000,
+          1000
+        ]
+      }
+      ,
+      {
+        shopId: 3,
+        shopName: "李四牛肉店",
+        amounts:[
+          1000,
+          2000,
+          1000,
+          2000,
+          1000,
+          2000,
+          2000,
+          1000,
+          2000,
+          1000,
+          2000,
+          2000,
+          1000
+        ]
+      },
+      {
+        shopId: 2,
+        shopName: "李四牛肉店",
+        amounts:[
+          1000,
+          2000,
+          1000,
+          2000,
+          1000,
+          2000,
+          2000,
+          1000,
+          2000,
+          1000,
+          2000,
+          2000,
+          1000
+        ]
+      }
+      ,
+      {
+        shopId: 3,
+        shopName: "李四牛肉店",
+        amounts:[
+          1000,
+          2000,
+          1000,
+          2000,
+          1000,
+          2000,
+          2000,
+          1000,
+          2000,
+          1000,
+          2000,
+          2000,
+          1000
+        ]
+      },
+      {
+        shopId: 2,
+        shopName: "李四牛肉店",
+        amounts:[
+          1000,
+          2000,
+          1000,
+          2000,
+          1000,
+          2000,
+          2000,
+          1000,
+          2000,
+          1000,
+          2000,
+          2000,
+          1000
+        ]
+      }
+      ,
+      {
+        shopId: 3,
+        shopName: "李四牛肉店",
+        amounts:[
+          1000,
+          2000,
+          1000,
+          2000,
+          1000,
+          2000,
+          2000,
+          1000,
+          2000,
+          1000,
+          2000,
+          2000,
+          1000
+        ]
+      },
+      {
+        shopId: 2,
+        shopName: "李四牛肉店",
+        amounts:[
+          1000,
+          2000,
+          1000,
+          2000,
+          1000,
+          2000,
+          2000,
+          1000,
+          2000,
+          1000,
+          2000,
+          2000,
+          1000
+        ]
+      }
+      ,
+      {
+        shopId: 3,
+        shopName: "李四牛肉店",
+        amounts:[
+          1000,
+          2000,
+          1000,
+          2000,
+          1000,
+          2000,
+          2000,
+          1000,
+          2000,
+          1000,
+          2000,
+          2000,
+          1000
+        ]
+      }
+    ]
+  }
+
 
   ngOnInit() {
+    // this.queryReport()
+    this.getAllMarkets()
+  }
+
+  queryReport() {
+    this.reportService.query(this.marketId, this.mounth).subscribe(
+      (report) => {
+       this.report = report
+      }
+    )
+  }
+
+  getAllMarkets() {
+    this.marketService.getAll().subscribe(
+      (markets) => {
+        this.markets = markets;
+      }
+    )
   }
 
 }
---
+++ src/app/manage/report/shared/report.model.ts
@@ -0,0 +1,10 @@
+export class MounthReport {
+  days: any[]
+  shops: Shop[]
+}
+
+export class Shop {
+  shopId: number
+  shopName: string
+  amounts: number[]
+}
