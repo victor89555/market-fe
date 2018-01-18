9b361f25553fa06e0f0b7c47f87407c23781c415
Victor <573864600@qq.com>
2018-01-18 10:48:04 +0800

    menubar更新

--- src/app/shared/menu-bar/menu-bar.component.ts
+++ src/app/shared/menu-bar/menu-bar.component.ts
@@ -74,7 +74,7 @@ export class MenuBarComponent implements OnInit {
       resource: "BLOCK_SYSTEM",
       children: [
         {title: "用户管理", link: "/manage/users", active: false, icon: "user1", resource: "MENU_USER"},
-        {title: "角色管理", link: "/manage/roles", active: false, icon: "profile", resource: "MENU_RULE"},
+        {title: "角色管理", link: "/manage/roles", active: false, icon: "profile", resource: "MENU_ROLE"},
         {title: "资源管理", link: "/manage/resources", active: false, icon: "box", resource: "MENU_RESOURCE"}
       ]
     }
