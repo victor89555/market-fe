3a2f1e100ec235f0f40b740f73f1687d9b341387
Victor <573864600@qq.com>
2018-01-23 17:20:54 +0800

    月度报表日期选择

--- src/app/manage/report/month-report/month-report.component.ts
+++ src/app/manage/report/month-report/month-report.component.ts
@@ -3,6 +3,7 @@ import {MounthReport} from "../shared/report.model";
 import {Market} from "../../market/shared/market.model";
 import {MarketService} from "../../market/shared/market.service";
 import {ReportService} from "../shared/report.service"
+import {formatDate} from "../../../thurder-ng/utils/date-util";
 
 @Component({
   selector: 'app-month-report',
@@ -17,11 +18,8 @@ export class MonthReportComponent implements OnInit {
 
   markets: Market[] = []
   marketId: number = null
-  now: number
-  pre: number
-  next: number
   preDay: string
-  nowDay: string
+  nowDay: string = formatDate(new Date(), 'yyyy-MM-dd').slice(0, 7)
   nextDay: string
   report: MounthReport = {
     days: ['01/01','01/02','01/03','01/04','01/05','01/06','01/07','01/08','01/09','01/10','01/11','01/12','01/13'],
@@ -282,26 +280,17 @@ export class MonthReportComponent implements OnInit {
     ]
   }
 
-  formatDateTOString() {
-    this.preDay = this.getYear(this.pre) + "-" + this.getMonth(this.pre)
-    this.nowDay = this.getYear(this.now) + "-" + this.getMonth(this.now)
-    this.nextDay = this.getYear(this.next) + "-" + this.getMonth(this.next)
-  }
-
-  getMonth(ts) {
-    let m = (new Date(ts).getMonth() + 1).toString()
-    m = m.length == 1 ? "0" + m : m
-    return m
-  }
-  getYear(ts) {
-    let y = new Date(ts).getFullYear().toString()
-    return y
-  }
-  setDay(ts) {
-    this.pre = ts - 2592000000
-    this.now = ts
-    this.next = ts + 2592000000
-    this.formatDateTOString()
+  setDay(str) {
+    this.nowDay = str
+    let now = new Date(str+'-01')
+    let preY = now.getFullYear()
+    let preM = now.getMonth()
+    preM < 1 && (preY--) && (preM += 12)
+    this.preDay = formatDate(new Date(preY.toString() + '-' + preM.toString() + '-01'),'yyyy-MM-dd').slice(0, 7)
+    let nextY = now.getFullYear()
+    let nextM = now.getMonth() + 2
+    nextM > 12 && (nextY++) && (nextM = nextM - 12)
+    this.nextDay = formatDate(new Date(nextY.toString() + '-' + nextM.toString() + '-01'),'yyyy-MM-dd').slice(0, 7)
     this.queryReport()
   }
 
@@ -309,7 +298,7 @@ export class MonthReportComponent implements OnInit {
     this.getAllMarkets().subscribe(
       (markets) => {
         this.marketId = markets[0].id
-        this.setDay(new Date().getTime())
+        this.setDay(this.nowDay)
       }
     )
   }
