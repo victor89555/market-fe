67c905cb3bb86ef2a87e1ca818dcbc8146903af4
chenshixiong <1067816765@qq.com>
2018-01-24 18:30:04 +0800

    报表修复暂存

---
+++ src/app/manage/market/repair-reports/repair-reports.component.ts
@@ -0,0 +1,34 @@
+import {Component, OnInit, EventEmitter} from '@angular/core';
+import {MarketService} from "../shared/market.service";
+import {formatDate} from "../../../thurder-ng/utils/date-util";
+
+@Component({
+  selector: 'app-repair-reports',
+  templateUrl: './repair-reports.component.html',
+  styleUrls: ['./repair-reports.component.scss']
+})
+export class RepairReportsComponent implements OnInit {
+  repairModal = {'beginTime': "", 'endTime': ""}
+  dismiss: EventEmitter<any>;
+
+  constructor(private marketService: MarketService) {
+  }
+
+  ngOnInit() {
+  }
+
+  repair() {
+    console.log("memem")
+    let bt = formatDate(new Date(this.repairModal.beginTime), "yyyy-MM-dd hh:mm:ss")
+    let et = formatDate(new Date(this.repairModal.endTime), "yyyy-MM-dd hh:mm:ss")
+    this.marketService.repair(bt, et)
+      .subscribe((repair) => {
+        console.log(repair)
+        this.dismiss.emit(repair)
+      })
+  }
+
+  cancel() {
+    this.dismiss.error(this.repairModal);
+  }
+}
